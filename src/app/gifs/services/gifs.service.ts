f7b647bfa7dbeeef704ee487bb5cbba8cf6ab65d
German Mendez <mendezgerman678@gmail.com>
2023-12-12 22:52:04 -0300

    Url giphy updated

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,34 +5,34 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 //ProvderIn indica en que secciones de mi codigo estara disponile, en este caso al ser root esta disponible en TODOS los archivo
 @Injectable({providedIn: 'root'})
 export class GifsService {
-    
+
 
     public gifList:Gif[]=[];
 
     private _tagsHistory    :string[]=[];
     private apiKey          :string = '16CTm4RdaVlH0i6Y06Rl0bvlpWfMH3Md';
-    private serviceGiphyURL :string='http://api.giphy.com/v1/gifs/';
+    private serviceGiphyURL :string='https://api.giphy.com/v1/gifs/';
 
     //HttpClient es una clase propia de Angular que me permite trabajar con peticiones, como en c#, esta clase debo importarla en el app.ts y luego en cada componente que vaya a usarlo
     private httpClient:HttpClient;
-    
+
     constructor(httpClient: HttpClient) {
         this.httpClient=httpClient;
         this.loadLocalStorage();
      }
 
-    
+
     public get tagsHistory() : string[] {
         //Creo una copia con el spread para no pasar la propiedad original ya que como se pasa por referencia esta puede ser modificada desde afuera de la clase y no quiero eso entonces paso una copia de tal forma que el array original no se vea afectado ante cambios
         return [...this._tagsHistory];
     }
-   
+
 
     //Metodo que se encarga de organizar mi array de historial. Valida que si el tag ya existe lo manda a la primer posicion en el historial, sino lo agrega y siempre mantiene un limite de maximo 10 elementos en el array
     private organizeHistory(tag:string){
         tag=tag.toLowerCase();
         if(this._tagsHistory.includes(tag)){
-            this._tagsHistory=this._tagsHistory.filter((element) => element !== tag);            
+            this._tagsHistory=this._tagsHistory.filter((element) => element !== tag);
         }
         this._tagsHistory.unshift(tag);
         this._tagsHistory=this._tagsHistory.slice(0,10);
@@ -71,6 +71,6 @@ export class GifsService {
           }
     }
 
-    
+
 }
 
